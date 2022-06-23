04cf453abe41eab240b1c2201e21c4c3f3aad34f
vanutcho123 <hainguyen.061001@gmail.com>
2022-06-23 15:15:32 +0700

    1.Create-react-app

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">H2SHOP</a>
+      </header>
+      <main>list product</main>
+    </div>
+  );
+}
+
+export default App;
