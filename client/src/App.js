8690cc0283f22d0d8902d7cf8dc3df49a074c094
vanutcho123 <hainguyen.061001@gmail.com>
2022-06-24 12:52:19 +0700

    React-route-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
-import "./App.css";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
+import ProductScreen from "./screens/ProductScreen";
+import HomeScreen from "./screens/HomeScreen";
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">H2SHOP</a>
-      </header>
-      <main>list product</main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">H2SHOP</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
